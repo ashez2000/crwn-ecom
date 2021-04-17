1416386f9232b25e1caad667b1d2507b413130c7
= <=>
2021-04-17 09:13:52 +0530

    header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css'
 
 import { Switch, Route } from 'react-router-dom'
 
-// pages
+// pages and components
 import Homepage from './pages/homepage/homepage.component'
 import Shop from './pages/shop/shop.component'
+
+import Header from './components/header/header.component'
 
 const App = () => {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={Shop} />
