d08013ac0d125e8093805c75822f0573f1984098
= <=>
2021-04-28 13:22:59 +0530

    cart dropdoen cart-item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,27 @@
 import './cart-dropdown.styles.scss'
 
-import CustomButton from '../custom-button/custom-button.component'
+import { connect } from 'react-redux'
 
-const CartDropdown = () => {
+import CustomButton from '../custom-button/custom-button.component'
+import CartItem from '../cart-item/cart-item.component'
+
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className='cart-dropdown'>
-      <div className='cart-items'></div>
+      <div className='cart-items'>
+        {cartItems.map(i => (
+          <CartItem key={i.id} item={i} />
+        ))}
+      </div>
       <CustomButton>Checkout</CustomButton>
     </div>
   )
 }
 
-export default CartDropdown
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  return {
+    cartItems,
+  }
+}
+
+export default connect(mapStateToProps)(CartDropdown)
